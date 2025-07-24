11578ca42144ba157a48db4c82474148b7f09b1e
Diego Quan <dquan@delicatessen502.com>
2025-07-24 08:02:59 -0600

    Clase abstracta para Food y extension function para food

--- src/Main.kt
+++ src/Main.kt
@@ -32,3 +32,26 @@ class IceCream(name: String, price: Double) : Food(name, price){
         return "El helado $name se come despacio mientras se va reduciendo el volumen"
     }
 }
+
+//Clase abstracta Drink que hereda de Food
+abstract class Drink(name: String, price: Double) : Food(name, price){
+    abstract fun pour(): String
+}
+
+//Subclase Juice que hereda de Drink
+class Juice(name: String, price: Double) : Drink(name, price){
+
+    override fun cook(): String {
+        return "Para exprimir el jugo $name se deben seleccionar frutas frescas y usar un extractor"
+    }
+
+    override fun pour(): String {
+        return "El jugo $name se vierte inclinando el bote donde esté poco a poco"
+    }
+}
+
+//Extension Function para Food
+fun Food.discountedPrice(discountedPercent: Double): Double {
+    val discount = price * (discountedPercent / 100)
+    return price - discount
+}
