1e41e8a000b6387242d4dc71ba97a40405752863
Diego Quan <dquan@delicatessen502.com>
2025-07-24 07:46:27 -0600

    Clase abstracta food y subclases pizza y burger

--- src/Main.kt
+++ src/Main.kt
@@ -1,1 +1,18 @@
-//Prueba de commit
+//Clase abstracta de comidas
+abstract class Food(val name: String, val price: Double){
+    abstract fun cook(): String
+}
+
+//Subclase burger
+class Burger(name: String, price: Double) : Food(name, price){
+    override fun cook(): String {
+        return "La hamburguesa $name se debe asar durante 15 minutos a fuego alto"
+    }
+}
+
+//Subclase burger
+class Pizza(name: String, price: Double) : Food(name, price){
+    override fun cook(): String {
+        return "La pizza $name se hornea hasta que las orillas se vean cafés"
+    }
+}
